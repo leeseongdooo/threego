10513a37f64b1315c84399740cc5fecdc1990ee9
leeseongdooo <tjdgns8966@naver.com>
2022-05-04 01:02:48 +0900

    Day0503(2);

--- src/Component/ReviewWrite.jsx
+++ src/Component/ReviewWrite.jsx
@@ -53,28 +53,59 @@ function ReviewWrite() {
         console.log(score);
     };
 
+    const [Image, setImage] = useState('asdfasdf')
+
+    const ReviewImage = (e) => {
+        const ReviewImg = e.target.files;
+        setImage(ReviewImg[0].name);
+    }
+
     return (
         <>
-            <header>
+            {/* 상단부분 */}
+            <header className="ReviewWriteHeader">
                 리뷰 작성 
             </header>
-            {/* console.log(active[0].active) */}
-            <div className="MiddleArea">
-                <div className="ScoreBox">
-                    {ARRAY.map((el, idx) => { // map을 통해 ARRAY값을 가져오고 
-                        return (
-                            <FaStar
-                            key={idx} // key = 0~4까지
-                            size="50" // 
-                            onClick={() => {handleStarClick(el);}} // 클릭시 handleStarClick 함수 실행 
-                            className={active[el] && 'yellowStar'} // 참이라면 yellowStar 활성화
-                            />
-                        );
-                    })}  
-                </div>
-                <h2>{active.filter(Boolean).length}</h2>
-            </div>
 
+            <div className="ReviewArea">
+                <div className="PointArea">
+                    <h3>여행은 어떠셨나요??</h3>
+                    <div className="ScoreBox">
+                        {ARRAY.map((el, idx) => { // map을 통해 ARRAY값을 가져오고 
+                            return (
+                                <FaStar
+                                key={idx} // key = 0~4까지
+                                size="50" // 
+                                onClick={() => {handleStarClick(el);}} // 클릭시 handleStarClick 함수 실행 
+                                className={active[el] && 'yellowStar'} // 참이라면 yellowStar 활성화
+                                />
+                            );
+                        })}  
+                    </div>
+                    {/* <h2>{active.filter(Boolean).length}</h2> */}
+                </div>
+
+                <div className="WriteArea">
+                    <input type="text" placeholder="제목을 입력해주세요" className="Title"/>
+                    <textarea name="" id="" className="WriteContent" ></textarea>
+                    
+                    <div className="ReviewWriteImageArea">
+                        <div className="ImageGuideText">
+                            <h5>사진 등록</h5>
+                        </div>
+                    
+                        <input type="file" id="image" accept="img/*" onChange={ReviewImage} />
+                        <br /> <br />
+                        <img src={"img/" + Image} alt="+" className="SampleImage" />
+                        <img src={"img/" + Image} alt="+" className="SampleImage" />
+                        <img src={"img/" + Image} alt="+" className="SampleImage" />
+                    </div>
+                </div>
+
+                <footer className="ReviewWriteBottom">
+                    <button onClick={() => {console.log("A")}}>등록하기</button>
+                </footer>
+            </div>
                 
         </>
     )
