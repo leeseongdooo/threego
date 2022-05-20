2060487563e5b8a15fa897386c6c929c289982c5
leeseongdooo <tjdgns8966@naver.com>
2022-05-20 17:32:09 +0900

    Day0520(1)

--- src/Component/ReviewManagement.jsx
+++ src/Component/ReviewManagement.jsx
@@ -1,4 +1,4 @@
-import {React, useState} from "react";
+import {React, useEffect, useState} from "react";
 import Header from "./Header";
 import Bottom from "./Bottom";
 import { FaStar } from 'react-icons/fa';
@@ -15,12 +15,18 @@ function ReviewForm ({List, setReviewManage, ReviewManage}) {
         ReviewStar.push(i);
     }
     
-
-  
-
     return (
         <div className="ReviewForm">
-            <input type="checkbox"/>
+            <input type="checkbox" onClick={(e)=>{
+                    if(List.checkBool == false)
+                        setReviewManage(ReviewManage.map(user => user.id == List.id  ? {...user, checkBool: true} : user));
+                    else if(List.checkBool == true) 
+                        setReviewManage(ReviewManage.map(user => user.id == List.id  ? {...user, checkBool: false} : user))
+
+                    } 
+                    
+
+                    } />
             <div className="MiddleTopArea">
                 <div className="NameAndScore">
                     <h3>{List.ReviewTitle}</h3>
@@ -30,7 +36,7 @@ function ReviewForm ({List, setReviewManage, ReviewManage}) {
 
                 <div className="MiddleDeleteBtn">
                     {/* <button>수정</button> */}
-                    <button onClick={()=>{setReviewManage(ReviewManage.filter((kk)=>kk.id !== List.id))}} >삭제</button>
+                    <button onClick={()=>{setReviewManage(ReviewManage.filter((kk)=>kk.id !== List.id)); console.log(ReviewManage)}} >삭제</button>
                 </div>
 
             </div>
@@ -47,7 +53,7 @@ function ReviewManagement() {
     const [ReviewManage, setReviewManage] = useState([
         {
             id: 1,
-            TravelName: "여행지 이름",
+            TravelName: "여행지 이름1",
             ReviewImage: [],
             ReviewDate: '2022.04.28',
             ReviewTitle: '이곳은 제목',
@@ -57,7 +63,7 @@ function ReviewManagement() {
         },
         {
             id: 2,
-            TravelName: "여행지 이름",
+            TravelName: "여행지 이름12",
             ReviewImage: [],
             ReviewDate: '2022.04.28',
             ReviewTitle: '이곳은 제목',
@@ -67,7 +73,7 @@ function ReviewManagement() {
         },
         {
             id: 3,
-            TravelName: "여행지 이름",
+            TravelName: "여행지 이름123",
             ReviewImage: [],
             ReviewDate: '2022.04.28',
             ReviewTitle: '이곳은 제목',
@@ -76,6 +82,11 @@ function ReviewManagement() {
             checkBool: false
         }
     ])
+
+    useEffect(()=>{
+        console.log(ReviewManage);
+    },[ReviewManage])
+  
 
     return (
         <div className="ReviewManagementBigBox">
@@ -92,7 +103,7 @@ function ReviewManagement() {
                                 <h4>총 {ReviewManage.length}개</h4>
                             </div>
 
-                            <h4 className="Delete">선택삭제</h4>
+                            <h4 className="Delete" onClick={()=>{setReviewManage(ReviewManage.filter((check)=>check.checkBool !== true))}}>선택삭제</h4>
 
                         </div>
                     </div>
@@ -100,9 +111,6 @@ function ReviewManagement() {
                     <div className="MiddleArea">
                       {ReviewManage.map((List)=>(<ReviewForm ReviewManage={ReviewManage} setReviewManage={setReviewManage} List={List} key={List.id}/>))}
                     </div>
-
-
-
                 </div>
             <Bottom/>
         </div>
